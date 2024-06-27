1eb490558c6b0822dbd5041b4227eb0f8659a0dc
SchrodingersCattt <gmy721212@163.com>
2024-06-27 20:57:18 +0800

    fix tests

--- crystalformerapp/tests/config.py
+++ crystalformerapp/tests/config.py
@@ -8,3 +8,15 @@ import jax
 import jax.numpy as jnp
 import numpy as np 
 import haiku as hk
+from jax.lib import xla_bridge
+# Check if GPU is available
+try:
+    if xla_bridge.get_backend().platform == 'gpu':
+        print("GPU is available. Using GPU.")
+    else:
+        raise RuntimeError("No GPU available, switching to CPU.")
+except RuntimeError as e:
+    print(e)
+    os.environ["JAX_PLATFORMS"] = "cpu"
+    jax.config.update("jax_platform_name", "cpu")
+    print("Changed platform to CPU.")
